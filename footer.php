2db050b4105640af3dea76ba790980d1bb5532c6
gledsondf <gledsondf@gmail.com>
2016-03-11 18:22:54 -0300

    Alteração do Css
    começando o layout

---
+++ footer.php
@@ -0,0 +1,43 @@
+</div><!--geral-->
+<div id="geralRodape" class="row">
+	<div id="contRodape" class="row">
+	<div id="acessorios">
+		<ul>
+			<li><a href="#topo" ><img title="ir para o topo"  alt="seta ir para o topo" src="<?php bloginfo('template_url'); ?>/img/setaTop.png" /></a></li>
+		</ul>
+	</div>
+		<footer>
+			<div id="enderecoRodape" class="col-2terco">
+				<address>
+					SEDE: Av. do Vale, Quadra 23, Lote 09, Sala 511. Ed. Carraras,<br /> Bairro: Renascença II   CEP 65075-660 - São Luís/MA.<br />Telefones:(98) 3304-7779 / 3304-7774.
+				</address>
+
+			</div>
+			<div id="midiaRodape" class="col-terco">
+				<ul>
+					<li><a href="https://www.facebook.com/Conselho-Regional-de-Fisioterapia-e-Terapia-Ocupacional-do-Maranh%C3%A3o-792759374172502/" target="_blank"><img title="facebook"  alt="facebook" src="<?php bloginfo('template_url'); ?>/img/logoFace.png" /></a></li>
+<!-- 					<li><a href="#"><img title="Youtube"  alt="Youtube" src="<?php bloginfo('template_url'); ?>/img/logoYoutube.png" /></a></li>
+ -->					<li><a href="https://twitter.com/crefito16" target="_blank"><img title="Twitter"  alt="Twitter" src="<?php bloginfo('template_url'); ?>/img/logoTwitter.png" /></a></li>
+					    <li><a href="https://instagram.com/crefito16/" target="_blank"><img title="Instagram"  alt="Instagram" src="<?php bloginfo('template_url'); ?>/img/logoInstagram.png" /></a></li>
+					<li><a href="https://www.flickr.com/photos/crefito16/" target="_blank"><img title="Flickr"  alt="Flickr" src="<?php bloginfo('template_url'); ?>/img/logoFlickr.png" /></a></li>
+					<!--<li><a href="#"><img title="Whatsapp"  alt="Whatsapp" src="<?php bloginfo('template_url'); ?>/img/logoWhatsapp.png" /></a></li> -->
+				</ul>
+				
+				
+			</div>
+		</footer>
+	</div>
+</div><!--geralRodape-->
+
+<script type="text/javascript">
+	function abreMenu() {
+		document.getElementById("menuN").style.display="block";
+	}
+	function fecharMenu() {
+		//alert("teste");
+		document.getElementById("menuN").style.display="none";
+	}
+</script>
+ <?php wp_footer(); ?> 
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<!-- div id="contMenuAcessoRapido" class="contMenuLateral">
+	<nav>
+		<h1>Acesso Rápido</h1>
+		<ul>
+			<?php wp_nav_menu( array( 'theme_location' => 'menu-acesso-rapido','items_wrap' => '%3$s','container' => '', ) ); ?>
+		</ul>
+	</nav>
+</div -->
+
+<div class="box">
+	<p>teste</p>
+</div>	
+<div class="box barra">
+	<p>teste2</p>
+</div>
+<div id="busca" class="row box barra">
+	<?php get_search_form(); ?>
+
+</div>	
+<div id="contMenuServico" class="contMenuLateral">
+	<nav>
+		<h1>Serviços</h1>
+		<ul>
+			<?php wp_nav_menu( array( 'theme_location' => 'menu-servicos','items_wrap' => '%3$s','container' => '', ) ); ?>
+		</ul>
+
+	</nav>
+</div>
+<div class="destaqueIconografico">
+<a href="<?php bloginfo( 'url' ) ?>/?p=434" title="<?php bloginfo( 'name' ) ?>" rel="home"><img src="<?php bloginfo('template_url'); ?>/img/denuncie.png" /></a>
+</div>
